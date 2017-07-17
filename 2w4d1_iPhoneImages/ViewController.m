3a6ca7962533ccf49b5d391bc6d7139ad9429e91
hkimca <harrison.kim.ca@gmail.com>
2017-07-17 14:19:45 -0400

    Complete (ex stretch)

--- 2w4d1_iPhoneImages/ViewController.m
+++ 2w4d1_iPhoneImages/ViewController.m
@@ -10,13 +10,41 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *iPhoneImageView;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // ***** TO KEEP THE SAFETY FEATURE OF HTTPS GO TO THE WEBPAGE AND INSPECT TO FIND THE URL PATH TO THE SPECIFIC IMAGE (USUALLY ADDS i. AND THE FILE TYPE TO THE HTTP LINK) *****
+    NSURL *url = [NSURL URLWithString:@"https://i.imgur.com/bktnImE.png"];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error)
+    {
+        if (error)
+        {
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // this will run on the main queue
+            self.iPhoneImageView.image = image;
+        }];
+    }];
+    
+    [downloadTask resume];
 }
 
 
