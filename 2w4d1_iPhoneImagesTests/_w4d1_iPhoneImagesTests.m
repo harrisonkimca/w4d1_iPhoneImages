f2ce0acc7501d0b632266ecd36d6f7996c791912
hkimca <harrison.kim.ca@gmail.com>
2017-07-17 11:47:23 -0400

    Initial Commit

---
+++ 2w4d1_iPhoneImagesTests/_w4d1_iPhoneImagesTests.m
@@ -0,0 +1,39 @@
+//
+//  _w4d1_iPhoneImagesTests.m
+//  2w4d1_iPhoneImagesTests
+//
+//  Created by Seantastic31 on 17/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _w4d1_iPhoneImagesTests : XCTestCase
+
+@end
+
+@implementation _w4d1_iPhoneImagesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
